c4d29aad23630f7b4c13eba1c40ae8b5583eb5b9
osawa.tomohisa <osawa.tomohisa07@gmail.com>
2025-06-18 14:49:47 +0900

    github icon add

--- components/Header.vue
+++ components/Header.vue
@@ -12,6 +12,22 @@
         <li class="header-item"><nuxt-link to="/#development" class="header-link">DEVELOPMENT</nuxt-link></li>
         <li class="header-item"><nuxt-link to="/projects" class="header-link">PROJECTS</nuxt-link></li>
         <li class="header-item"><nuxt-link to="/#tool-skill" class="header-link">TOOL & SKILL</nuxt-link></li>
+        <li class="header-item"> <a href="https://github.com/osawa1177/nuxt-portfolio-site" target="_blank"
+            rel="noopener noreferrer">
+            <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clip-path="url(#clip0_416_1723)">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M16 0.985352C7.16 0.985352 0 8.14535 0 16.9854C0 24.0654 4.58 30.0454 10.94 32.1654C11.74 32.3054 12.04 31.8254 12.04 31.4054C12.04 31.0254 12.02 29.7654 12.02 28.4254C8 29.1654 6.96 27.4454 6.64 26.5454C6.46 26.0854 5.68 24.6654 5 24.2854C4.44 23.9854 3.64 23.2454 4.98 23.2254C6.24 23.2054 7.14 24.3854 7.44 24.8654C8.88 27.2854 11.18 26.6054 12.1 26.1854C12.24 25.1454 12.66 24.4454 13.12 24.0454C9.56 23.6454 5.84 22.2654 5.84 16.1454C5.84 14.4054 6.46 12.9654 7.48 11.8454C7.32 11.4454 6.76 9.80535 7.64 7.60535C7.64 7.60535 8.98 7.18535 12.04 9.24535C13.32 8.88535 14.68 8.70535 16.04 8.70535C17.4 8.70535 18.76 8.88535 20.04 9.24535C23.1 7.16535 24.44 7.60535 24.44 7.60535C25.32 9.80535 24.76 11.4454 24.6 11.8454C25.62 12.9654 26.24 14.3854 26.24 16.1454C26.24 22.2854 22.5 23.6454 18.94 24.0454C19.52 24.5454 20.02 25.5054 20.02 27.0054C20.02 29.1454 20 30.8654 20 31.4054C20 31.8254 20.3 32.3254 21.1 32.1654C27.42 30.0454 32 24.0454 32 16.9854C32 8.14535 24.84 0.985352 16 0.985352Z"
+                  fill="white" />
+              </g>
+              <defs>
+                <clipPath id="clip0_416_1723">
+                  <rect width="32" height="32" fill="white" transform="translate(0 0.985352)" />
+                </clipPath>
+              </defs>
+            </svg>
+          </a></li>
+
         <li class="header-item"><nuxt-link to="/contact" class="header-btn">
             <span class="button-caption">相談・お問い合わせ</span>
           </nuxt-link></li>
